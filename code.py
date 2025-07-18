f46ac97e03074627a1ad5460757bcbecb5ad3701
Syeda Hira Fatima <syedahirafatima11@gmail.com>
2025-07-18 15:34:52 +0500

    code.py

---
+++ code.py
@@ -0,0 +1,8 @@
+# Get input from the user
+letter = input("Enter a lowercase letter: ")
+
+# Convert to uppercase
+uppercase_letter = letter.upper()
+
+# Print the result
+print("Uppercase:", uppercase_letter)
